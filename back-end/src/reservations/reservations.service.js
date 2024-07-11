daaac02d15d346e96c8117de4d7bb57b332cfc66
Adan <jonesadan@yahoo.com>
2024-07-11 14:31:41 -0400

    Updated file of typos, incorrect ues of  method and and ensured the  is properly handled when it's not provided

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,17 +1,22 @@
 const knex = require("../db/connection.js");
 
-function list (reservation_date) {
-  return knex("reservations")
+function list(reservation_date) {
+  const query = knex("reservations")
     .select("*")
-    .where({ reservation_date })
     .whereNot({ status: "finished" })
     .orderBy("reservation_time");
+  
+  if (reservation_date) {
+    query.where({ reservation_date });
+  }
+  
+  return query;
 }
 
 function search(mobile_number) {
   return knex("reservations")
     .whereRaw(
-      "translate(mobile_numer, '() -', '') like ?",
+      "translate(mobile_number, '() -', '') like ?",
       `%${mobile_number.replace(/\D/g, "")}%`
     )
     .orderBy("reservation_date");
@@ -32,17 +37,19 @@ function update(updatedRes) {
   console.log("updatedRes", updatedRes);
   return knex("reservations")
     .select("*")
-      .where({ resevation_id: updatedRes.resevation_id })
-      .update(updateRes, "*")
-      .then((createdRecords) => createdRecords[0]);
+    .where({ reservation_id: updatedRes.reservation_id })
+    .update(updatedRes)
+    .returning("*")
+    .then((updatedRecords) => updatedRecords[0]);
 }
 
-function updateStatus (reservation_id, status) {
-  return knex ("resevations")
+function updateStatus(reservation_id, status) {
+  return knex("reservations")
     .select("*")
     .where({ reservation_id })
-    .update({ status: status }, "*")
-    .then((createdRecords) => createdRecords[0]);
+    .update({ status: status })
+    .returning("*")
+    .then((updatedRecords) => updatedRecords[0]);
 }
 
 module.exports = {
