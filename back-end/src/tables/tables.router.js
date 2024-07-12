c80d2a8b918ef8e2f414eea182b8b2c6a8af584a
Adan <jonesadan@yahoo.com>
2024-07-11 22:21:09 -0400

    Add routing for tables with create, list, update, and finish endpoints
    - Implement POST and GET routes for creating and listing tables
    - Implement PUT and DELETE routes for updating and finishing table seating
    - Handle method not allowed errors for unsupported HTTP methods

---
+++ back-end/src/tables/tables.router.js
@@ -0,0 +1,17 @@
+const router = require("express");
+const controller = require("./tables.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+router
+  .route("/")
+  .post(controller.create)
+  .get(controller.list)
+  .all(methodNotAllowed);
+
+  router
+    .route("/:tables_id/seat")
+    .put(controller.create)
+    .delete(controller.finish)
+    .all(methodNotAllowed);
+
+module.exports = router;
